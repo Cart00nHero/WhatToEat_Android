d82a7ef3a1a91b0b5cfe0bea7b1267f38211c704
Morris <maurice@mitake.com.tw>
2020-11-17 10:39:36 +0800

    update

--- WhatToEat/app/src/main/java/com/cartoonhero/source/stage/scenery/listItems/AddGLRItemLayout.kt
+++ WhatToEat/app/src/main/java/com/cartoonhero/source/stage/scenery/listItems/AddGLRItemLayout.kt
@@ -17,7 +17,6 @@ class AddGLRItemLayout @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : LRItemLayout(context, attrs, defStyleAttr) {
 
-    var itemHeight = 0
     fun buildLayout() {
         this.leftLayout.setBackgroundColor(Color.parseColor("#F5FFFA"))
         createLeft()

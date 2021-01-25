f7927211fd16950eb4c9f3a949deb53130ef845b
Morris <maurice@mitake.com.tw>
2021-01-25 11:22:35 +0800

    Update

---
+++ WhatToEat/app/src/main/java/com/cartoonhero/source/actors/timeGuardian/TimeUtils.kt
@@ -0,0 +1,31 @@
+package com.example.dbs_android_notifications_inbox.utils
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+object TimeGuardian {
+    @JvmStatic
+    fun timeStampToDate(time: Long, locale: Locale): String {
+        //val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", locale)
+        val simpleDateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm", locale)
+        return simpleDateFormat.format(Date(time))
+    }
+    @JvmStatic
+    fun dateToTimeStamp(date: String, locale: Locale): Long {
+        val simpleDateFormat = SimpleDateFormat("yyyy/MM/dd HH:mm", locale)
+        /// 輸出為毫秒為單位
+        return simpleDateFormat.parse(date).time
+    }
+
+    fun String.toDate(dateFormat: String = "yyyy/MM/dd HH:mm", timeZone: TimeZone = TimeZone.getTimeZone("UTC")): Date {
+        val parser = SimpleDateFormat(dateFormat, Locale.getDefault())
+        parser.timeZone = timeZone
+        return parser.parse(this)
+    }
+
+    fun Date.formatTo(dateFormat: String, timeZone: TimeZone = TimeZone.getDefault()): String {
+        val formatter = SimpleDateFormat(dateFormat, Locale.getDefault())
+        formatter.timeZone = timeZone
+        return formatter.format(this)
+    }
+}
